fa5e01dbcde6ae7c839b3b438fa9c6b80adddf9f
Harry Brundage <harry.brundage@gmail.com>
2015-05-26 21:04:41 -0400

    Initial commit

---
+++ formidable-hubspot-api.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Formidable Hubspot API
+Description: Create hubspot customers on the Forms API via Formidable Forms
+Version: 0.1
+Plugin URI: http://ottawacondonetwork.com
+Author: Harry Brundage
+Author URI: http://harry.me
+*/
+
+function frm_forms_api_autoloader($class_name) {
+    // Only load Frm classes here
+    if ( ! preg_match('/^FrmHubAPI.+$/', $class_name) ) {
+        return;
+    }
+
+    $filepath = dirname(__FILE__);
+    if ( preg_match('/^.+Helper$/', $class_name) ) {
+        $filepath .= '/helpers/';
+    } else if ( preg_match('/^.+Controller$/', $class_name) ) {
+        $filepath .= '/controllers/';
+    } else {
+        $filepath .= '/models/';
+    }
+
+    $filepath .= $class_name .'.php';
+
+    if ( file_exists($filepath) ) {
+        include($filepath);
+    }
+}
+
+// Add the autoloader
+spl_autoload_register('frm_forms_api_autoloader');
+
+FrmHubAPIAppController::load_hooks();
+FrmHubAPISettingsController::load_hooks();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hacky Formidable Wordpress plugin for sending form entries to Hubspot Forms
+
+Forked from formidable-api, https://formidablepro.com/knowledgebase/formidable-api/
